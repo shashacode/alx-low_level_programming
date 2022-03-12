a03a9a16628557ef29207f9cd78071f570e021a3
Shashacode <floraoladipupo@gmail.com>
2022-03-11 16:14:26 -0800

    Second commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
 	{
-		printf("greater than 5");
+		printf("greater than 5\n");
 	}
 	else if (n % 10 == 0)
 	{	
-		printf("0");
+		printf("0\n");
 	}
 	else
 	{
-		printf("less than 6 and not 0");
+		printf("less than 6 and not 0\n");
 	}
 	return (0);
 
